35cb14aac8ab7cd19d817175fbdb51350bacfb4e
Moha35abu <153958870+Moha35abu@users.noreply.github.com>
2026-02-06 18:16:12 +0300

    fix base path for github pages

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
@@ -7,9 +8,10 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
