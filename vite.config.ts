955bf21c7f67884c4b92776f0c51772116d0d71f
Moha35abu <153958870+Moha35abu@users.noreply.github.com>
2026-02-06 17:41:06 +0300

    fix base path for github pages (2)

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
 
   return {
-    base: "/Special-Edu-Ai/", // ✅ ADD THIS LINE
+    base: "/Special-Edu-Ai",
 
     server: {
       port: 3000,
