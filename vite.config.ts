73a9a346c611ac3cf1c78e028c50d6bd443de0e5
Moha35abu <153958870+Moha35abu@users.noreply.github.com>
2026-02-07 14:57:50 +0300

    k

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,13 @@
 import path from "path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-//NEW
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, ".", "");
-
-  return {
-    base: "/Special-Edu-Ai/",
-
-    server: {
-      port: 3000,
-      host: "0.0.0.0",
+export default defineConfig({
+  base: "/Special-Edu-Ai/",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "."),
     },
-
-    plugins: [react()],
-
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "."),
-      },
-    },
-  };
+  },
 });
