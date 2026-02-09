cf5ce5511ab918dd0dc0d29c23dc249f16297ecc
Moha35abu <153958870+Moha35abu@users.noreply.github.com>
2026-02-09 22:59:54 +0300

    final

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,23 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/Special-Edu-Ai/",
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "."),
-    },
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-  define: {
-    "import.meta.env.VITE_GEMINI_API_KEY": JSON.stringify(
-      process.env.NODE_ENV === "production"
-        ? ""
-        : process.env.VITE_GEMINI_API_KEY || "",
-    ),
-  },
+  return {
+    base: "/Special-Edu-Ai/",
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "."),
+      },
+    },
+
+    define: {
+      "import.meta.env.VITE_GEMINI_API_KEY": JSON.stringify(
+        mode === "production" ? "" : env.VITE_GEMINI_API_KEY || "",
+      ),
+    },
+  };
 });
